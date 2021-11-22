7e814045d2ddeb327e218c244650eaa4a2abd89c
FunnyDev8149 <94663363+FunnyDev8149@users.noreply.github.com>
2021-11-22 06:07:48 +0300

    Vue-Nuxt.js Template

---
+++ pages/elements/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<main class="main">
+		<h1 class="d-none">Funny Vue eCommerce Template</h1>
+
+		<div class="page-content">
+			<element-list></element-list>
+		</div>
+	</main>
+</template>
+
+<script>
+import ElementList from '~/components/partials/elements/ElementList';
+
+export default {
+	components: {
+		ElementList
+	}
+}
+</script>
